89cd5d16eb782fd3e4ffc97c1a04b72d3ee58e94
Namanpreet Ghotra <ng00451@surrey.ac.uk>
2023-03-08 00:53:27 +0000

    Frame created

---
+++ src/GamePanel.java
@@ -0,0 +1,27 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JPanel;
+
+public class GamePanel extends JPanel implements ActionListener{
+	
+	GamePanel(){
+		
+	}
+	
+	
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Grahpics g) {
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
