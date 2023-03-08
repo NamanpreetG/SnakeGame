958df5db3b8fbe87999331def14b28a1f22eef57
Namanpreet Ghotra <ng00451@surrey.ac.uk>
2023-03-08 01:09:31 +0000

    Added fields and constructor ro the gamePanel class

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -3,11 +3,45 @@ import java.awt.*;
 import java.awt.event.*;
 
 import javax.swing.*;
+
+import java.util.Random;
 import java.util.random.*;
 
 public class GamePanel extends JPanel implements ActionListener{
 	
+	
+	static final int SCREEN_WIDHT = 600;
+	static final int SCREEN_HEIGHT = 600;
+	static final int UNIT_SIZE = 25;
+	static final int GAME_UNITS = (SCREEN_WIDHT*SCREEN_HEIGHT)/UNIT_SIZE;
+	static final int DELAY = 75;
+	
+	//These arrays store the snakes location and size
+	final int x[]= new int[GAME_UNITS];
+	final int y[]= new int[GAME_UNITS];
+	//size of the snake
+	int bodyParts = 6;
+	int applesEaten = 0 ;
+	//location of the apple 
+	int appleX;
+	
+	
+	//This the direction the snake travels, R for right, L for left, D for down and U for up
+	char direction = 'R';
+	
+	boolean running = false;
+	Timer timer;
+	Random random;
+	
+	
+	
 	GamePanel(){
+		random = new Random();
+		this.setPreferredSize(new Dimension(SCREEN_WIDHT, SCREEN_HEIGHT));
+		this.setBackground(Color.black);
+		this.setFocusable(true);
+		this.addKeyListener(new MyKeyAdapter());
+		startGame();
 		
 	}
 	
